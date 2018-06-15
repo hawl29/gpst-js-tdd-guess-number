20734c5360661a1ecbcbe98e2120285888f08a6f
hawl29 <cz_hawl29@163.com>
2018-06-15 15:23:12 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,6 +1,31 @@
 // Write your cade below:
-function main() {};
+function main(guess_number)
+{
+    var right_number='1234';
+    var result=[0,0];
+    var patten=/^(?!\d*?(\d)\d*?\1)\d{4}$/;
+    if(patten.test(guess_number))          //判断字符串是4位无重复数字
+    {
+      for(let i=0;i<right_number.length;i++)    //外层循环遍历系统给出的答案
+      {
+        for(let j=0;j<guess_number.length;j++)  //内层循环遍历用户猜测的数字
+        {
+            if(right_number[i]===guess_number[i])   //对应位置数字相同则记为A
+            {
+                result[0]++;
+                break;
+            }
+            else if(right_number[i]===guess_number[j])   //数字相同但位置不同则记为B
+            {
+                result[1]++;
+                break;
+            }
+        }
+      }
+      return result[0]+'A'+result[1]+'B';
+    }
+    else return '非法输入';
+};
 
-module.exports = {
-    main
-}
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,52 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('guess_number', function() {
+    it('全对', function()
+    {
+        let guess_number='1234'
+        let result = main(guess_number);
+        expect(result).toEqual('4A0B');
+    })
 
+    it('错，数字不同', function()
+    {
+        let guess_number='5678'
+        let result = main(guess_number);
+        expect(result).toEqual('0A0B');
+    })
+
+    it('错，顺序不同', function()
+    {
+        let guess_number='4213'
+        let result = main(guess_number);
+        expect(result).toEqual('1A3B');
+    })
+
+    it('错，部分数字同，部分顺序同', function()
+    {
+        let guess_number='1563'
+        let result = main(guess_number);
+        expect(result).toEqual('1A1B');
+    })
+
+    it('非法输入,重复数字', function()
+    {
+        let guess_number='1134'
+        let result = main(guess_number);
+        expect(result).toEqual('非法输入');
+    })
+
+    it('非法输入,非四位数', function()
+    {
+        let guess_number='12345'
+        let result = main(guess_number);
+        expect(result).toEqual('非法输入');
+    })
+
+    it('非法输入,非数字字符', function()
+    {
+        let guess_number='1a23'
+        let result = main(guess_number);
+        expect(result).toEqual('非法输入');
+    })
 });
